5209ab882015a80d261cb32999ff5678c4465324
AdamPach <adam.pach@outlook.cz>
2021-12-02 09:32:16 +0100

    Add log account and some checks

--- PasswordManagerLib/Core.cs
+++ PasswordManagerLib/Core.cs
@@ -1,4 +1,5 @@
 using PasswordManagerLib.Crypto;
+using PasswordManagerLib.Exeption;
 using PasswordManagerLib.Manipulators;
 using PasswordManagerLib.Models;
 
@@ -15,15 +16,39 @@ public class Core : ICore
         //Must create file for this account where you save all passwords
 
         var Accounts = await GetAccountsAsync();
+
+        if(CheckDuplicitNames(Accounts, NewAccount))
+            throw new DuplicitAccountNaneExeption("This name is exist!");
+
         Accounts.Add(NewAccount);
         await WriteAccountsAsync(Accounts);
-
         return NewAccount;
     }
 
-    public bool LogIn(string Name, string Password)
+    private Account LogedAccount;
+
+    public bool IsLogged()
     {
-        throw new NotImplementedException();
+        return LogedAccount != null ? true : false;
+    }
+
+    public async Task<bool> LogIn(string Name, string Password)
+    {
+        List<Account> accounts = await GetAccountsAsync();
+
+        Account accountToLogin = (from a in accounts where a.Name == Name select a).FirstOrDefault();
+
+        if(accountToLogin == null)
+            throw new NoExistingAccountExeption("You enter a name which is not existing!");
+        
+        IPasswordComparator comparator = new PasswordComparator(new SHA256PasswordHasher());
+        if(await comparator.ComparePassword(accountToLogin.Password, Password))
+        {
+            LogedAccount = accountToLogin;
+            return true;
+        }
+        
+        return false;
     }
 
     private async Task<List<Account>> GetAccountsAsync()
@@ -39,6 +64,13 @@ public class Core : ICore
         manipulator = new XmlAccountsManipulator();
         await manipulator.WriteAccounst(accounts, Statics.AccountsFile);
         FreeMemory();
+    }
+
+    private bool CheckDuplicitNames(IEnumerable<Account> accounts, Account NewAccount)
+    {
+        var account = from a in accounts where NewAccount.Name == a.Name select a;
+        System.Console.WriteLine(account.Count());
+        return account.Count() > 0;
     }
 
     private void FreeMemory()
--- PasswordManagerLib/ICore.cs
+++ PasswordManagerLib/ICore.cs
@@ -5,5 +5,7 @@ namespace PasswordManagerLib;
 public interface ICore
 {
     public Task<Account> CreateAccount(string Name, string Password);
-    public bool LogIn(string Name, string Password);
+    public Task<bool> LogIn(string Name, string Password);
+
+    public bool IsLogged();
 }
